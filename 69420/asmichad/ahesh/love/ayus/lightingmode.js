17af9090617d4b462189b788cd5c1c7de01aa0f0
ShinyACash <76147605+ShinyACash@users.noreply.github.com>
2022-12-08 13:36:06 +0530

    1.1

--- 69420/asmichad/ahesh/love/ayus/lightingmode.js
+++ 69420/asmichad/ahesh/love/ayus/lightingmode.js
@@ -1,3 +1,9 @@
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
 });
+
+var r = document.getElementById("rick")
+
+r.addEventListener("click", function () {
+    window.location.replace("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+});
